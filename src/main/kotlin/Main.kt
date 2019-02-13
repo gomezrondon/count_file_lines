9ba0e08ce472a6828f4cd60cc508b8559331154b
Javier Gomez <Jgomez@cambridgeassociates.com>
2019-02-13 08:34:07 -0600

    1rst Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,26 @@
 import java.io.File
 
 fun main(args : Array<String>) {
-    var stashName = args[0].toString().toLowerCase()
+    val measureTimeMillis = kotlin.system.measureTimeMillis {
+        var stashName = args[0].toLowerCase()
+    //Calculation
+      val fileCounts = File(stashName).walk()
+                .filter { it.isFile }
+                .map { Pair(it.name, it.readLines().count()) }
 
-    val lines = File(stashName).readLines()
+    //Presentation
+        val size = fileCounts.map { it.first.length }.max()
 
-    val regexExec ="""\*\s(.)+""".toRegex(RegexOption.MULTILINE)
+        var repeatNum = size;
+        println("File"+ repeatNum?.let { " ".repeat(it) } +"count")
+        println("===="+ repeatNum?.let { "=".repeat(it) } +"=====")
 
-    lines.flatMap { regexExec.findAll(it).map { it.value }.toList() }
-            .map{it.replace("* ","")}
-            .forEach { println(it) }
+        for ((name, count) in fileCounts) {
+            repeatNum = size?.minus(name.length);
+            System.out.printf("%s " + repeatNum?.let { " ".repeat(it) } + " %5d\n", name, count)
+        }
+    }
+    println("\n total time:$measureTimeMillis milliseconds")
 }
+
+// java -jar count_file_lines.jar "C:\temp\compose\java"
